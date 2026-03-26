d9cce6787f3b79f63a1d25f0b57838d080c268c8
yurikses <yurikses@gmail.com>
2026-03-26 09:30:16 +0300

    feat: basic structure + sw file

---
+++ tasks-list/public/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "list-cache-v1";
+
+const URLS_TO_CACHE = ["/", "/index.html"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(URLS_TO_CACHE);
+    }),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name)),
+      );
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    }),
+  );
+});
---
+++ tasks-list/src/main.tsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log("SW зарегистрирован! Scope: ", registration.scope);
+      })
+      .catch((err) => {
+        console.log("Ошибка регистрации SW: ", err);
+      });
+  });
+}
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
